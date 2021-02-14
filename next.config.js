fd39e0d9029770f2a719d78e51111e4412c474e5
SivaramPg <sivarampg95@gmail.com>
2021-02-14 11:51:16 +0530

    next config file with redirects

---
+++ next.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/m/:id',
+        destination: '/api/:id',
+        permanent: false,
+      },
+      {
+        source: '/gh',
+        destination: 'https://github.com/SivaramPg',
+        permanent: true,
+      },
+      {
+        source: '/view-source',
+        destination: 'https://github.com/SivaramPg/url-masker',
+        permanent: true,
+      },
+    ];
+  },
+};
