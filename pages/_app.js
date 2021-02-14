605158f136e4e712a76050260db9eaa7f07d4bbc
SivaramPg <sivarampg95@gmail.com>
2021-02-14 11:54:17 +0530

    updated _app file

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,46 @@
-import '../styles/globals.css'
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  *,
+  *::before,
+  *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: inherit;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+
+  @font-face {
+    font-family: 'Wotfard';
+    src: url('/wotfard-regular-webfont.eot');
+    src: url('/wotfard-regular-webfont.eot?#iefix') format('embedded-opentype'),
+    url('/wotfard-regular-webfont.ttf') format('truetype'),
+    url('/wotfard-regular-webfont.woff2') format('woff2');
+  }
+
+  body {
+    box-sizing: border-box;
+    font-family: 'wotfard', -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+`;
+
+const theme = {};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyle />
+      <ThemeProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
