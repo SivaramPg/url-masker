a0f6241099ee5cc7ebe62b593e8e3b4e1c26ccac
SivaramPg <sivarampg95@gmail.com>
2021-02-14 12:02:04 +0530

    main api handler for returning original url mapped to id

---
+++ pages/api/[id].js
@@ -0,0 +1,27 @@
+import db from '../../utils/db';
+
+export default async function handler(req, res) {
+  try {
+    if (req.method === 'GET') {
+      const {
+        query: { id },
+      } = req;
+
+      if (id && id.length === 5) {
+        const mapping = await db.collection('mapping').doc(id).get();
+        const data = mapping.data();
+
+        if (data) {
+          return res.redirect(data.url, 307);
+        } else {
+          return res.status(404).json({ error: 'Not found' });
+        }
+      }
+    } else {
+      return res.status(404).json({ error: 'Not found' });
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error.message || error });
+  }
+}
